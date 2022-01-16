c9e171497b446f52cb58036df8687ca5c331da06
Lalugilang015 <lalugilangsukmadinata@gmail.com>
2022-01-16 15:19:37 +0700

    first commit

---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,89 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Ayam;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing(ctx.getString(R.string.angora_nama), ctx.getString(R.string.angora_asal),
+                ctx.getString(R.string.angora_deskripsi), R.drawable.cat_angora));
+        kucings.add(new Kucing(ctx.getString(R.string.bengal_nama), ctx.getString( R.string.bengal_asal),
+                ctx.getString(R.string.bengal_deskripsi), R.drawable.cat_bengal));
+        kucings.add(new Kucing(ctx.getString(R.string.birmani_nama), ctx.getString(R.string.birmani_asal),
+                ctx.getString(R.string.birmani_deskripsi), R.drawable.cat_birman));
+        kucings.add(new Kucing(ctx.getString(R.string.persia_nama), ctx.getString(R.string.persia_asal),
+                ctx.getString(R.string.persia_deskripsi), R.drawable.cat_persia));
+        kucings.add(new Kucing(ctx.getString(R.string.siam_nama), ctx.getString(R.string.siam_asal),
+                ctx.getString(R.string.siam_deskripsi), R.drawable.cat_siam));
+        kucings.add(new Kucing(ctx.getString(R.string.siberia_nama), ctx.getString(R.string.siberia_asal),
+                ctx.getString(R.string.siberia_deskripsi), R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing(ctx.getString(R.string.bulldog_nama), ctx.getString(R.string.bulldog_asal),
+                ctx.getString(R.string.bulldog_deskripsi), R.drawable.dog_bulldog));
+        anjings.add(new Anjing(ctx.getString(R.string.husky_nama), ctx.getString(R.string.husky_asal),
+                ctx.getString(R.string.husky_deskripsi), R.drawable.dog_husky));
+        anjings.add(new Anjing(ctx.getString(R.string.kintamani_nama), ctx.getString(R.string.kintamani_asal),
+                ctx.getString(R.string.kintamani_deskripsi), R.drawable.dog_kintamani));
+        anjings.add(new Anjing(ctx.getString(R.string.samoyed_nama), ctx.getString(R.string.samoyed_asal),
+                ctx.getString(R.string.samoyed_deskripsi), R.drawable.dog_samoyed));
+        anjings.add(new Anjing(ctx.getString(R.string.shepherd_nama), ctx.getString(R.string.shepherd_asal),
+                ctx.getString(R.string.shepherd_deskripsi), R.drawable.dog_shepherd));
+        anjings.add(new Anjing(ctx.getString(R.string.shiba_nama), ctx.getString(R.string.shiba_asal),
+                ctx.getString(R.string.shiba_deskripsi), R.drawable.dog_shiba));
+        return anjings;
+    }
+
+    private static List<Ayam> intDataAyam(Context ctx) {
+        List<Ayam> ayams = new ArrayList<>();
+        ayams.add(new Ayam(ctx.getString(R.string.kampung_nama), ctx.getString(R.string.kampung_asal),
+                ctx.getString(R.string.kampung_deskripsi), R.drawable.ayamkampung));
+        ayams.add(new Ayam(ctx.getString(R.string.kedo_nama), ctx.getString(R.string.kedo_asal),
+                ctx.getString(R.string.kedo_deskripsi), R.drawable.ayamkedu));
+        ayams.add(new Ayam(ctx.getString(R.string.ketawa_nama), ctx.getString(R.string.ketawa_asal),
+                ctx.getString(R.string.ketawa_deskripsi), R.drawable.ayamketawa));
+        ayams.add(new Ayam(ctx.getString(R.string.pelung_nama), ctx.getString(R.string.pelung_asal),
+                ctx.getString(R.string.pelung_deskripsi), R.drawable.ayampelung));
+        return ayams;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(intDataAyam(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
